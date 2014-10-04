527eea4e5171ca5d6c5486c2bb808585f6efa657
Alexander Garner <acgarner@mail.smu.edu>
2014-10-03 23:16:11 -0500

    added list.html.erb and established connection with database

--- Proj4.1/albums.rb
+++ Proj4.1/albums.rb
@@ -1,12 +1,13 @@
 require 'rack'
+require 'sqlite3'
 
-class	#domain object 
-attr_accessor
-def initialize(:rank, :title, :year)
-	@rank = rank
-	@title = title
-	@year = year
-end
+#class	#domain object 
+#attr_accessor
+#def initialize(:rank, :title, :year)
+#	@rank = rank
+#	@title = title
+#	@year = year
+#end
 
 class AlbumApp
 
@@ -30,7 +31,6 @@ end
 	def render_form(request)
 		response= Rack::Response.new
 		response.write(ERB.new(File.read("form.html.erb")).result(binding))
-		#File.open("form.html.erb","rb") {|form| response.write(form.read)}
 		response.finish
 	end
 
@@ -46,27 +46,31 @@ end
 		# 	<td><%=album.rank%></td>
 		#  ... etc
 		#
-
-		File.open("top.html", "rb") {|form| response.write(form.read)}
 		
-		File.open("top_100_albums.txt", "r").each do |line|
-   			split_line = line.chomp.split(", ")
-   			dataArray.push(split_line)
-		end
-		# TODO: sort an array based on the request parameter called sortBy
 
+		#File.open("top.html", "rb") {|form| response.write(form.read)}
+		
+		
+		
 		sortBy = request.params["sortBy"]
 		rank = request.params["selectNumber"].to_i
 
-		if sortBy == "Name"
-			dataArray.sort! { |l, r| l[0] <=> r[0] }
-		elsif sortBy == "Year"
-			dataArray.sort! { |l, r| l[1] <=> r[1] }
-		end
+		database = SQLite3::Database.open("albums.sqlite3.db")
+		#dataArray = database.execute("SELECT * FROM albums ORDER BY #{sortBy}")
 		
 
+		if sortBy == "Name"
+			#dataArray.sort! { |l, r| l[0] <=> r[0] }
+		dataArray = database.execute("SELECT * FROM albums ORDER BY title")
+		elsif sortBy == "Year"
+			#dataArray.sort! { |l, r| l[1] <=> r[1] }
+			dataArray = database.execute("SELECT * FROM albums ORDER BY year")
+		end
 
-		response.write("<table border=\"1\">\n")
+		database.close
+
+
+		#response.write("<table border=\"1\">\n")
 		dataArray.each_with_index do |album, i|
 			response.write("\t<tr>\n")
 			counter = i + 1
@@ -75,22 +79,20 @@ end
 			if counter == rank
 				string = "\t\t<tr class = \"highlight\">"
 				string = string + "\t\t<td>" + counter.to_s + "</td>\n"
-				string = string + "\t\t<td>" + album[0] + "</td>\n"
-				string = string + "\t\t<td>" + album[1] + "</td>\n"
+				string = string + "\t\t<td> #{album[1]} </td>\n"
+				string = string + "\t\t<td> #{album[2]} </td>\n"
 				response.write(string)
 			else
 
 				response.write("\t\t<td>" + counter.to_s + "</td>\n")
-				response.write("\t\t<td>" + album[0] + "</td>\n")
-				response.write("\t\t<td>" + album[1] + "</td>\n")
+				response.write("\t\t<td> #{album[1]} </td>\n")
+				response.write("\t\t<td> #{album[2]} </td>\n")
 			end
 			response.write("\t</tr>\n")
 		end
 		response.write("</table>\n")
 
 		File.open("bottom.html", "rb") {|form| response.write(form.read)}
-#http://stackoverflow.com/questions/10490204/capture-dynamic-list-to-an-array-using-ruby
-		#response.write(contents)
 		response.finish
 	end
 
