1c80d4e6b9b20d9d3339f04c25c77b58ea80502c
Alexander Garner <acgarner@mail.smu.edu>
2014-10-16 23:55:57 -0500

    set up rails project-before modification

--- project4.2/albums.rb
+++ project4.2/albums.rb
@@ -3,6 +3,18 @@ require 'data_mapper'
 require_relative 'album'
 DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/albums.sqlite3.db")
 set :port, 8080
+
+get "/form" do
+	erb :form
+end
+
+post "/list" do
+	@sort_order = params[:sorts] # <- fix the :sorts to match my code
+	@highlight_to_highlight = params[:rank]
+	@albums = Album.all(:order => @sort_order.to_sym)
+	erb :list
+end
+
 get "/" do
   "Sinatra is working!"
 end
